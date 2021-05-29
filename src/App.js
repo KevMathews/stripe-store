0749581c64d466f9d59376459b8cf5592dd3a574
Kevin Mathews <kev.math@yahoo.com>
2021-05-29 16:58:33 -0700

    created hero component from bulma

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Header from './components/header/header.jsx';
+import Header from './components/header/header';
+import Hero from './components/hero/hero';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,21 @@
+import './hero.styles.scss';
+
+const Hero = () => {
+  return (
+    <section className='hero is-info is-large'>
+      <div className='hero-body'>
+        <div className='container'>
+          <h1 className='title'>
+            Bags reimagined for modern life.
+          </h1>
+          <div className='shop-now-btn'>
+            <button className='button is-black' id='shop-now'>
+              SHOP NOW
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+export default Hero;
