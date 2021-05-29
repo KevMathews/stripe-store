0a9c0afa6177738de840265c8b6c213663dd333d
Kevin Mathews <kev.math@yahoo.com>
2021-05-29 16:07:52 -0700

    creating initial file structure and header component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import './header.styles.scss';
+
+const Header = () => {
+  return (
+    <nav className='nav-menu container'>
+      <div className='logo'>
+        <Link to='/'>MyStore</Link>
+      </div>
+      <ul>
+        <li>
+          <Link to='/'>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='/shop'>
+            Shop
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+};
+
+export default Header;
