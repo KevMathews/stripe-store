cd6cc7d569d830223234698690df243c5322dd41
Kevin Mathews <kev.math@yahoo.com>
2021-06-03 01:21:17 -0700

    started wiring up stripe to the frontend

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import ProductsContextProvider from './context/products-context';
 import CartContextProvider from './context/cart-context';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
+
 ReactDOM.render(
   <BrowserRouter>
     <ProductsContextProvider>
       <CartContextProvider>
-        <App />
-      </CartContextProvider>
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
+=     </CartContextProvider>
     </ProductsContextProvider>
   </BrowserRouter>,
  
