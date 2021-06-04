c3cc6b4f5e37d1694d23a8d92cbbadc95d7a6fec
Kevin Mathews <kev.math@yahoo.com>
2021-06-03 22:53:38 -0700

    success and canceled pages created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Shop from './components/pages/shop/shop';
 import SingleProduct from './components/single-product/single-product';
 import CartPage from './components/pages/cart-page/cart-page';
 import Checkout from './components/checkout/checkout';
+import Success from './components/checkout/stripe-checkout/success';
+import Canceled from './components/checkout/stripe-checkout/canceled';
 import './App.scss';
 
 
@@ -17,6 +19,9 @@ function App() {
         <Route path='/product/:id' component={SingleProduct} />
         <Route path='/cart' component={CartPage} />
         <Route path='/checkout' component={Checkout} />
+        <Route path='/success' component={Success} />
+        <Route path='/canceled' component={Canceled} />
+
         <Route path='*' component={NotFound} />
       </Switch>
     </div>
